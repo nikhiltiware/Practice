1dba72ee8049e41bdb52d796ec0c76c712dab8f9
Nikhil Tiware <nikhiltiware@gmail.com>
2016-02-29 12:19:43 -0500

    alldepthsofTrees

---
+++ src/com/company/TreeNode.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by Nikhil on 2/29/16.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x){ val=x;}
+
+}
---
+++ src/com/company/allDepthsoftree.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by Nikhil on 2/29/16.
+ */
+public class allDepthsoftree {
+
+
+    public void level(TreeNode root){
+        List<LinkedList<TreeNode>> list = new ArrayList<LinkedList<TreeNode>>();
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+
+        while(!q.isEmpty())
+        {
+            LinkedList<TreeNode> l = new LinkedList<TreeNode>();
+            int size = q.size();
+            for(int i=0; i<size; i++)
+            {
+                TreeNode temp = q.remove();
+                l.add(temp);
+                if(temp.left!=null)q.add(temp.left);
+                if(temp.right!=null)q.add(temp.right);
+
+            }
+            list.add(l);
+
+        }
+
+
+    }
+}
