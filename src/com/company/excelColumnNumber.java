c4a357214e80d690b178bf0b87ee34f1178b100f
Nikhil Tiware <nikhiltiware@gmail.com>
2016-03-01 00:25:10 -0500

    excelColumnNumber

--- src/com/company/excelColumnNumber.java
+++ src/com/company/excelColumnNumber.java
@@ -8,8 +8,8 @@ public class excelColumnNumber {
     public int getColumnNumber(String s){
         int result =0;
         for(int i=0; i<s.length(); i++){
-            result = result*26;
-            result = result + s.charAt(i)-'A'+1;
+
+            result = result*26 + s.charAt(i)-'A'+1;
         }
 
         return result;
